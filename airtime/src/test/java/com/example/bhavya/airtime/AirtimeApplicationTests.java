f6d9afae57b362040c2f2cae62419746c14401cf
bhavyapaliwal2003 <143644054+bhavyapaliwal2003@users.noreply.github.com>
2024-07-25 21:39:19 +0530

    Add files via upload

---
+++ airtime/src/test/java/com/example/bhavya/airtime/AirtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bhavya.airtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
